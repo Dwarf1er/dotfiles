ac238fe80bf1dd1511edc65836248c0a8247a8c1
Antoine Poulin <SerolTech@protonmail.com>
2023-09-30 22:24:12 +0000

    Update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,1 +1,13 @@
--- Main config
+-- Linux/MacOS: vim.cmd("set runtimepath+=~/.config/nvim")
+-- Windows:
+vim.cmd("set runtimepath+=%USERPROFILE%\\AppData\\Local\\nvim")
+
+require("settings")
+
+require("mappings")
+
+require("autocmds")
+
+require("plugins")
+
+require("lsp")
