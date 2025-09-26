07d3e86fd813809f0cf077ef89629d4251d7dbfb
Dwarf1er <seroltech@protonmail.com>
2025-09-26 11:06:13 -0400

    feat: updated dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -11,13 +11,17 @@ rm -f $HOME/setup.sh
 git clone --bare "$git_repo_url" $HOME/.config
 
 config() {
-  /usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME "$@"
+    /usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME "$@"
 }
 
 config checkout
 
 config config --local status.showUntrackedFiles no
 
-sudo cp "$HOME/.config/system-config/boot/loader/loader.conf" /boot/loader/loader.conf
+sudo rm /boot/loader/loader.conf
+sudo ln -s "$HOME/.config/system-config/boot/loader/loader.conf" /boot/loader/loader.conf
+
+sudo rm /etc/tlp.conf
+sudo ln -s ~/.config/tlp/tlp.conf /etc/tlp.conf
 
 log_info "Dotfiles setup complete!"
