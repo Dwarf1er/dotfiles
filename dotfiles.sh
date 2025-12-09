d4b95c50f43f6d53e8d55f7f02f6cb97f3d89e44
Dwarf1er <seroltech@protonmail.com>
2025-12-09 17:19:51 -0500

    fix: dotfiles.sh missing log_info and local usage outside function

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-local git_repo_url="$1"
+
+git_repo_url="$1"
+
+log_info() {
+    echo -e "\033[0;32m[INFO]\033[0m $1"
+}
 
 log_info "Setting up dotfiles..."
 
-rm -rf $HOME/.config
-rm -f $HOME/.bashrc
-rm -f $HOME/README.md
-rm -f $HOME/setup.sh
+rm -rf "$HOME/.config"
+rm -f "$HOME/.bashrc" "$HOME/README.md" "$HOME/setup.sh"
 
-git clone --bare "$git_repo_url" $HOME/.config
+git clone --bare "$git_repo_url" "$HOME/.config"
 
 config() {
-    /usr/bin/git --git-dir=$HOME/.config/ --work-tree=$HOME "$@"
+    /usr/bin/git --git-dir="$HOME/.config/" --work-tree="$HOME" "$@"
 }
 
-config checkout
+config checkout -f
 
 config config --local status.showUntrackedFiles no
 
-sudo rm /boot/loader/loader.conf
-sudo ln -s "$HOME/.config/system-config/boot/loader/loader.conf" /boot/loader/loader.conf
+sudo mkdir -p /boot/loader
+sudo rm -f /boot/loader/loader.conf
+sudo ln -sf "$HOME/.config/system-config/boot/loader/loader.conf" /boot/loader/loader.conf
 
-sudo rm /etc/tlp.conf
-sudo ln -s ~/.config/tlp/tlp.conf /etc/tlp.conf
+sudo rm -f /etc/tlp.conf
+sudo ln -sf "$HOME/.config/tlp/tlp.conf" /etc/tlp.conf
 
 log_info "Dotfiles setup complete!"
